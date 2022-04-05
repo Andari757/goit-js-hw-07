93d7b19c87149290e9bf21d8e4cd353d2c09a63f
Andari757 <matviyskrypka@gmail.com>
2022-04-05 21:05:18 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const items = galleryItems
+    .map(e => `<div class="gallery__item">
+  <a class="gallery__link" href="${e.original}">
+    <img
+      class="gallery__image"
+      src="${e.preview}"
+      data-source="${e.original}"
+      alt="${e.description}"
+    />
+  </a>
+</div>`)
+    .join("");
+gallery.insertAdjacentHTML("afterbegin", items)
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+    return;
+    }
+    console.log(e.target.dataset.source)
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" >
+`)
+
+instance.show()
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const items = galleryItems
+    .map(e => `<a class="gallery__item" href="${e.original}">
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+</a>`)
+    .join("");
+gallery.insertAdjacentHTML("afterbegin", items)
+    
+    let lightbox = new SimpleLightbox('.gallery a',{captionsData: `alt`, captionDelay:250});
